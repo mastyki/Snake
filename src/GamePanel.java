c41c8a58bc408d90c4f7fe92c5e7c22814510445
lizam <liza.mastykina@gmail.com>
2022-01-22 23:11:35 +0300

    Initial commit

---
+++ src/GamePanel.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+
+    }
+    public void startGame(){
+
+    }
+    public  void printComponent(Graphics graphics){
+
+    }
+    public void draw (Graphics graphics){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollision(){
+
+    }
+    public void gameOver(Graphics graphics){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+            
+        }
+    }
+}
